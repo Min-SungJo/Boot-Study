5ec0825d818211dce15c2ff82005735c1699f3de
Min-SungJo <123or@naver.com>
2022-11-07 23:32:49 +0900

    google api 환경설정

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
